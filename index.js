bb426e2d1658985a462b682d1163d98da8a048c1
MH-RaZa-Developer <webdevelopermehedi1@gmail.com>
2021-10-28 10:07:53 +0600

    Changed Port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6000;
 
 app.use(cors());
 app.use(express.json());
